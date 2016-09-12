5303d16c5482b60e78c58fd5f3f331a54e1fe122
Hywel Carver <hywelc@gmail.com>
2016-09-12 08:10:53 +0100

    Fix emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,14 @@ Rails.application.configure do
       sender_address: %{"notifier" <notifier@example.com>},
       exception_recipients: (ENV['EXCEPTION_EMAIL_RECIPIENTS'] || '').split(',')
     }
+
+    ActionMailer::Base.smtp_settings = {
+      address:        'smtp.sendgrid.net',
+      port:           '587',
+      authentication: :plain,
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
+      domain:         'heroku.com',
+      enable_starttls_auto: true
+    }
 end
